c1f6bca2e8a82845ea7df0b5e2d96991ba819deb
MatiasDeiCastelliFL <matiasdeicastellifl@gmail.com>
2022-11-27 16:15:34 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-
+server.set("port",3001||process.env.PORT)
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
